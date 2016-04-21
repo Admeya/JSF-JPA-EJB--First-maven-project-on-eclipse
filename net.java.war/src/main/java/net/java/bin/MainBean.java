ba1f345392c64f7660bffd9d80fec92df106cc07
Admeya <lugovova@live.ru>
2016-04-21 12:58:23 +0300

    Работающий проект JSF

--- net.java.war/src/main/java/net/java/bin/MainBean.java
+++ net.java.war/src/main/java/net/java/bin/MainBean.java
@@ -1,33 +1,51 @@
 package net.java.bin;
 
-import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
-import javax.faces.context.FacesContext;
 
-@ManagedBean(name = "mainBean")
-@SessionScoped
+@ManagedBean // аннотация указывающая что это managed bean и поэтому мы его не описываем в faces-config.xml
+@SessionScoped // аннотация указывающая что это managed bean является сессионным
 public class MainBean {
+    private String userLogin;
+    private String password;
+    private String text;
 
-    private String inputText;
 
-    public String getInputText() {
-        return inputText;
+    public String getUserLogin() {
+        return userLogin;
     }
 
-    public void setInputText(String inputText) {
-        this.inputText = inputText;
+    public void setUserLogin(String userLogin) {
+        this.userLogin = userLogin;
     }
 
-    public void showMessage() {
-        FacesMessage message = new FacesMessage("Заголовок", "Частичное обновление страницы");
-        message.setSeverity(FacesMessage.SEVERITY_INFO); // как выглядит окошко с сообщением
-        FacesContext.getCurrentInstance().addMessage(null, message);
 
-        FacesContext.getCurrentInstance().addMessage(null,
-                new FacesMessage(FacesMessage.SEVERITY_ERROR, "Всплывашка", "GrowlMessage"));
+    public String getPassword() {
+        return password;
+    }
 
-        FacesContext.getCurrentInstance().addMessage(null,
-                new FacesMessage(FacesMessage.SEVERITY_FATAL, "Значение", inputText));
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+    // Метод используемый нами в качестве простейшей авторизации, проверяет имя и пароль пользователя и возвращает имя
+    // страницы на которую произойдет переход
+    public String checkUser() {
+        if (this.userLogin.equals("user") && this.password.equals("123")) {
+            return "success";
+        } else {
+            return "loginfailed";
+        }
+    }
+
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
     }
 }
